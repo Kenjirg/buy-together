5d068aea9260a7f6fdc0d033b64b2626f1a4e82b
Kenji <imkenjiasia@gmail.com>
2019-04-26 17:35:23 +0800

    prepare fb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190416084721) do
+ActiveRecord::Schema.define(version: 20190426082254) do
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20190416084721) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.text "uid"
+    t.string "name"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
