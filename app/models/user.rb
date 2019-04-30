b525fa2aa3d680ed4741dab5c4a724e7f5f404ec
Kenji <imkenjiasia@gmail.com>
2019-04-30 16:32:59 +0800

    e

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable#,
          #:omniauthable, :omniauth_providers => [:facebook]
  #  def self.from_omniauth(auth)
 	# where(provider: auth.provider, did: auth.uid).first_or_create do |user|
