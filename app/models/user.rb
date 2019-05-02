8858488932a0dc1999013655c0313797f8d2eabe
Kenji <imkenjiasia@gmail.com>
2019-05-02 17:55:06 +0800

    e

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
-	def from_omniauth(auth)
+	def self.from_omniauth(auth)
 		where(provider: auth.provider, did: auth.uid).first_or_create do |user|
 			user.email = auth.info.email
 			user.password = Devise.friendly_token[0,20]
