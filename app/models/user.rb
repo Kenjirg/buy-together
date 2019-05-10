7ad3b0584fda6f2ac114c03d51c1e4ce76851b10
Kenji <imkenjiasia@gmail.com>
2019-05-10 14:33:09 +0800

    uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 	def self.from_omniauth(auth)
-		where(provider: auth.provider, did: auth.uid).first_or_create do |user|
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 			user.email = auth.info.email
 			user.password = Devise.friendly_token[0,20]
 			user.name = auth.info.name # assuming the user model has a name
