e2ce88a961a6ada319dd21351db0526d8d3db339
Kenji <imkenjiasia@gmail.com>
2019-05-23 15:21:59 +0800

    item model ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'pages#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :stores
+  resources :stores do
+  	resources :items
+  end
   get '/user_ck' => 'pages#user_ck',as: 'user_ck'
 end
