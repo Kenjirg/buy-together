0d89d35ca70d6454e7868fe0a28420500bcf7063
Kenji <imkenjiasia@gmail.com>
2019-05-23 17:13:07 +0800

    item crud done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 	before_action :set_store
+	before_action :set_item, only: [:edit, :update, :destroy]
 	def create
 		if @store.items.create item_params
 			redirect_to store_path(@store)
@@ -7,10 +8,27 @@ class ItemsController < ApplicationController
 			render 'stores/show'
 		end
 	end
+	def edit		
+	end
+	def update
+		if @item.update item_params
+			redirect_to store_path(@store)
+		else
+			render 'edit'			
+		end
+	end
+	def destroy
+		@item.destroy
+		redirect_to store_path(@store)
+	end
+	private
 	def item_params
 		params.require(:item).permit(:name, :price, :image)
 	end
 	def set_store
 		@store=Store.find params[:store_id]
+	end
+	def set_item
+		@item=Item.find params[:id]
 	end
 end
