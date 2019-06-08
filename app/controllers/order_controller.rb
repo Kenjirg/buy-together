a48040a96bd185af0288aa103c3898d422546a3f
Kenji <imkenjiasia@gmail.com>
2019-06-08 13:48:33 +0800

    orders

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,19 +1,2 @@
 class OrderController < ApplicationController
-	before_action :authenticate_user!
-	before_action :set_purchase
-	def update
-		@order=Order.find params[:id]
-		# if @order.update order_params
-		# 	redirect_to purchase_path(@purchase)
-		# else
-		# 	render 'purchase/show'
-		# end
-	end
-	private
-	def set_purchase
-		@purchase=Purchase.find params[:purchase_id]
-	end
-	def order_params
-		# params.require(:order).permit()
-	end
 end
